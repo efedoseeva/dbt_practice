9e3f3e7741bda5bf399270227fbf0684649e4c66
Lena Fedoseeva <lena.fedoseeva@gmail.com>
2025-03-14 01:10:02 +0300

    new tables

--- models/staging/flights/stg_flights__bookings.sql
+++ models/staging/flights/stg_flights__bookings.sql
@@ -1,11 +1,19 @@
 {{
     config(
-        materialized='table'
+        materialized='materialized_view',
+        on_configuration_change="apply",
+        indexes=[
+        {
+            "columns": ["book_date"],
+            "unique": false
+        }
+    ]
     )
 }}
 select
         book_ref,
         book_date,
-        total_amount
+        total_amount,
+        total_amount*2+10 as some_amount
 from {{source('demo_src', 'bookings')}}  
     
